faf951b7974cb54b556002c9fd9c431ebc3b8a7c
M4rcoToni <marcosanto.sl021@academico.ifsul.edu.br>
2022-10-13 12:34:00 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './styles/global.css';
+
+export function App() {
+  return (
+    <div>    
+      <h1 className='font-bold text-5xl text-violet-500'>Hellow Word</h1>
+
+      <button className='bg-violet-500 font-medium px-4 py-2 rounded text-white hover:bg-violet-700'>
+      Enviar
+      </button>
+    </div>
+
+  )
+}
+
+
